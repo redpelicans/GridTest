7d864809395e1cfbb7f73d32e7641b694a6455bb
Europe assistance <ttridon@gmail.com>
2019-05-08 10:15:53 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Chip from '@material-ui/core/Chip';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import FlagIcon from './FlagIcon';
 
-function App() {
+const datas = [
+  {
+    firstname: 'Thomas',
+    lastname: 'Tridon',
+    country: 'fr',
+    phoneNumber: '06 00 00 00 00',
+    mail: 'thomas.tridon@redpelicans.com',
+    assets: [
+      'UI/UX',
+      'ReactJS',
+      'NodeJS'
+    ],
+    status: 'Pending',
+    rating: 5,
+    reporter: {
+      firstname: 'Eric',
+      lastname: 'Basley'
+    }
+  }
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container>
+      <Card>
+        <CardHeader
+          action={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          avatar={
+            <Avatar>
+              <Typography>TT</Typography>
+            </Avatar>
+          }
+          title={
+            <Fragment>
+              <Typography>Thomas Tridon</Typography>
+              <FlagIcon
+                code='fr'
+              />
+              <Typography>PhoneNumber</Typography>
+              <Link underline='always'>Mail</Link>
+            </Fragment>
+          }
+        />
+        <CardContent spacing={8}>
+          <Grid container spacing={8}>
+            <Grid item>
+              <Chip color='primary' label='UI/UX' variant='outlined' />
+            </Grid>
+            <Grid item>
+              <Chip color='primary' label='ReactJS' variant='outlined' />
+            </Grid>
+            <Grid item>
+              <Chip color='primary' label='NodeJS' variant='outlined' />
+            </Grid>
+          </Grid>
+        </CardContent>
+        <CardContent>
+          <Button
+            fullWidth
+            variant='contained'
+          >
+            <Typography>
+              Pending
+            </Typography>
+          </Button>
+        </CardContent>
+        <CardActions>
+          <Grid container alignItems='center' justify='space-between'>
+            <Grid item>
+              <Typography>Eric Basley</Typography>
+            </Grid>
+            <Grid item>
+              <IconButton>
+                <MoreVertIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 }
 
